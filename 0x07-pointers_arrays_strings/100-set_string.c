e37c2566e791b5622a14f7eb6e363f65d9bcf945
jovintus <jovintusmn@gmail.com>
2022-09-26 14:36:45 -0700

    i can set string to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - function with two arguments
+ * @s: pointer to string
+ * @to: pointer to string
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
