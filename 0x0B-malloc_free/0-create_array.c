1faf3a218761b3f834698ddef1bcd45e78cb8703
jovintus <jovintusmn@gmail.com>
2022-10-05 11:23:06 -0700

    we create the pro

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,28 +1,28 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* create_array - creates an array of chars, and
-* initializes it with a specific char.
-*@size: Size of the array
-*@c: Character to insert
-*Return: NULL if size is zero or if it fails,
-*pointer to array if everything is normal.
-*/
+ * *create_array - creates an array and init it with a specific char
+ * @size: the size of the array
+ * @c: the array
+ * Return: Null if size = 0, pointer or Null if it fails
+ */
 
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int index;
+	char *str;
+	unsigned int i = 0;
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char) * size);
-
-	if (array == NULL)
+	str = malloc(size * sizeof(*str));
+	if (str == NULL)
 		return (NULL);
 
-	for (index = 0; index < size; index++)
-		array[index] = c;
+	for (; i < size; i++)
+	{
+		str[i] = c;
+	}
 
-	return (array);
+	return (str);
 }
